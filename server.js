15d017b4c969f867b61e93c94316fb89ddb66799
gabe hollander <gabehollander@gmail.com>
2018-10-21 18:34:05 -0400

    gitignore

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ var express = require('express');
 var path = require("path");
 var bodyParser = require('body-parser');
 var mongo = require("mongoose");
+var fs = require('fs');
+
 
 var db = mongo.connect("mongodb://localhost:27017/connis", function(err, response){
   if(err){console.log(err);}
   else{console.log('Connected to ' + db, ' + ', response);}
 });
 
+// var bucket = new mongodb.GridFSBucket(db);
+
 var app = express();
+app.use(express.json({limit: '50mb'}));
 app.use(bodyParser());
-app.use(bodyParser.json({limit:'5mb'}));
+// app.use(bodyParser.json({limit:'10mb'}));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(function (req, res, next){
@@ -23,16 +28,35 @@ app.use(function (req, res, next){
 
 var Schema = mongo.Schema;
 
+// var BlogSchema = new Schema({
+//   title: { type: String },
+//   content: { type: String },
+// },{versionKey: false });
+
 var BlogSchema = new Schema({
   title: { type: String },
+  // content: { data: Buffer, contentType: String },
   content: { type: String },
+  content2: { type: String },
+  bodyText: { type: String }
+
 },{versionKey: false });
 
 var model = mongo.model('blog', BlogSchema, 'blog');
 
+var currentID;
+
 app.post("/api/savePost",function(req,res){
   var mod = new model(req.body);
+
+  console.log(req.title);
+  console.log(req.bodyText);
   if(req.body.mode == "Save"){
+
+    mod.title = req.body.title;
+    mod.content = req.body.content;
+    mod.content2 = req.body.content2;
+    mod.bodyText = req.body.bodyText;
     mod.save(function(err,data){
       if(err){
         res.send(err);
@@ -41,9 +65,10 @@ app.post("/api/savePost",function(req,res){
         res.send({data:"Post has been added!"});
       }
     });
+
   }
   else{
-    model.findByIdAndUpdate(req.body.id, { name: req.body.name, address: req.body.address},
+    model.findByIdAndUpdate(req.body.id, { title: req.body.title, content: req.body.content, content2: req.body.content2},
     function(err,data){
       if(err) {
         res.send(err);
@@ -67,8 +92,35 @@ app.post("/api/deletePost",function(req,res){
 })
 
 app.get("/api/getPost", function(req,res){
-  model.find({},function(err,data){
+  model.findOne({},function(err,data){
     if(err){
+      res.send(err);
+    }
+    else{
+      res.send(data);
+    }
+  });
+})
+
+app.post("/api/getNextPost", function(req,res){
+  // var current = model.find({"_id":req.body.id});
+  var data = model.find({_id: {$gt: req.body.id}}).sort({_id: 1}).limit(1).exec(function(err,data){
+    // console.log(data[0].title);
+    if(err){
+      res.send(err);
+    }
+    else{
+      res.send(data);
+    }
+  });
+})
+
+app.post("/api/getPreviousPost", function(req,res){
+  // var current = model.findOne(req.body.id);
+  var data = model.find({_id: {$lt: req.body.id}}).sort({_id: -1}).limit(1).exec(function(err,data){
+  // model.findOne({date: {$lt: current.date}}, {}, {sort: {date: -1}},function(err,data){
+    // console.log(data[0].title);
+    if(!data){
       res.send(err);
     }
     else{
