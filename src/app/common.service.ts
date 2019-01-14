48f57a7a506d3a6adb387069f0ed61c0df5c0088
gabe hollander <gabehollander@gmail.com>
2019-01-13 21:55:04 -0500

    almost

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -14,36 +14,36 @@ export class CommonService {
 
   //***************CLOTHING FUNCTIONS*****************
   deleteMerchandise(id){
-    return this.http.post('https://connis.herokuapp.com//api/deleteMerchandise/',{'id': id})
+    return this.http.post('https://connis.herokuapp.com/api/deleteMerchandise/',{'id': id})
       .map((response: Response) => response.json())
   }
   saveMerchandise(post){
-    return this.http.post('https://connis.herokuapp.com//api/saveMerchandise',post)
+    return this.http.post('https://connis.herokuapp.com/api/saveMerchandise',post)
       .map((response: Response) => response.json())
   }
   getMerchandise(){
-    return this.http.get('https://connis.herokuapp.com//api/getMerchandise/')
+    return this.http.get('https://connis.herokuapp.com/api/getMerchandise/')
       .map((response: Response) => response.json())
   }
   createCharge(token){
-    return this.http.post('https://connis.herokuapp.com//api/createCharge',token)
+    return this.http.post('https://connis.herokuapp.com/api/createCharge',token)
       .map((response: Response) => response.json())
   }
   //**************************************************
 
   //***************VIDEO FUNCTIONS****************
   saveVideo(post){
-    return this.http.post('https://connis.herokuapp.com//api/saveVideo',post)
+    return this.http.post('https://connis.herokuapp.com/api/saveVideo',post)
       .map((response: Response) => response.json())
   }
 
   getVideo(){
-    return this.http.get('https://connis.herokuapp.com//api/getVideo/')
+    return this.http.get('https://connis.herokuapp.com/api/getVideo/')
       .map((response: Response) => response.json())
   }
 
   deleteVideo(id){
-    return this.http.post('https://connis.herokuapp.com//api/deleteVideo/',{'id': id})
+    return this.http.post('https://connis.herokuapp.com/api/deleteVideo/',{'id': id})
       .map((response: Response) => response.json())
   }
   //**********************************************
@@ -51,17 +51,17 @@ export class CommonService {
   //**************MUSIC FUNCTIONS*****************
 
   saveMusic(post){
-    return this.http.post('https://connis.herokuapp.com//api/saveMusic',post)
+    return this.http.post('https://connis.herokuapp.com/api/saveMusic',post)
       .map((response: Response) => response.json())
   }
 
   getMusic(){
-    return this.http.get('https://connis.herokuapp.com//api/getMusic/')
+    return this.http.get('https://connis.herokuapp.com/api/getMusic/')
       .map((response: Response) => response.json())
   }
 
   deleteMusic(id){
-    return this.http.post('https://connis.herokuapp.com//api/deleteMusic/',{'id': id})
+    return this.http.post('https://connis.herokuapp.com/api/deleteMusic/',{'id': id})
       .map((response: Response) => response.json())
   }
 
@@ -70,27 +70,27 @@ export class CommonService {
   //********************BLOG FUNCTIONS***************
 
   savePost(post){
-    return this.http.post('https://connis.herokuapp.com//api/savePost',post)
+    return this.http.post('https://connis.herokuapp.com/api/savePost',post)
       .map((response: Response) => response.json())
   }
 
   getPost(){
-    return this.http.get('https://connis.herokuapp.com//api/getPost/')
+    return this.http.get('https://connis.herokuapp.com/api/getPost/')
       .map((response: Response) => response.json())
   }
 
   getNextPost(id){
-    return this.http.post('https://connis.herokuapp.com//api/getNextPost',{'id': id})
+    return this.http.post('https://connis.herokuapp.com/api/getNextPost',{'id': id})
       .map((response: Response) => response.json())
   }
 
   getPreviousPost(id){
-    return this.http.post('https://connis.herokuapp.com//api/getPreviousPost',{'id': id})
+    return this.http.post('https://connis.herokuapp.com/api/getPreviousPost',{'id': id})
       .map((response: Response) => response.json())
   }
 
   deletePost(id){
-    return this.http.post('https://connis.herokuapp.com//api/deletePost/',{'id': id})
+    return this.http.post('https://connis.herokuapp.com/api/deletePost/',{'id': id})
       .map((response: Response) => response.json())
   }
 
